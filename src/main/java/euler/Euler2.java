dcff3cad66c0199b8d1a38be00675f21ae02162e
Mantu Singh <mantusingh90@gmail.com>
2016-03-01 01:28:26 +0530

    structure change

---
+++ src/main/java/euler/Euler2.java
@@ -0,0 +1,25 @@
+package euler;
+
+import java.util.stream.BaseStream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
+
+
+public class Euler2 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    // System.out.println(IntStream.range(1, 500).map(x -> fibonacci(x)).filter(x -> x < 4000000 && x % 2 == 0).sum());
+    System.out.println(IntStream.iterate(1, x -> fibonacci(x) + 1).filter(x -> x < 4000000 && x % 2 == 0).sum());
+  }
+
+  public static int fibonacci(int number) {
+    if (number == 1 || number == 2) {
+      return 1;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2); // tail recursion
+  }
+
+
+}
---
+++ src/main/java/euler/Euler3.java
@@ -0,0 +1,35 @@
+package euler;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class Euler3 {
+
+  public static IntPredicate isPrime = n -> n < 4
+      || !(n % 2 == 0 || IntStream.range(2, (int) Math.sqrt(n) + 1).filter(x -> x % 2 != 0 && n % x == 0).findAny()
+          .isPresent());
+
+  public static void main(String[] args) {
+    System.out.println(isPrime.test(5));
+    // TODO Auto-generated method stub
+    // primeFactor(20).stream().forEach(System.out::println);
+
+    int n = 6;
+    IntStream.range(2, (int) Math.sqrt(n) + 1).filter(x -> n % x == 0).filter(isPrime).forEach(System.out::println);
+  }
+
+  static Set<Long> primeFactor(long num) {
+
+    Set<Long> factors = new HashSet<>();
+    for (long i = 2; i <= num; i++) {
+      while (num % i == 0) {
+        factors.add(i);
+        num /= i;
+      }
+    }
+    return factors;
+  }
+
+}
