3fa39a2153ae0ebb0151a05fd07ad9fb8da682af
Mantu Singh <mantusingh90@gmail.com>
2016-04-20 20:24:02 +0530

    adding fix

--- src/main/java/euler/Euler3.java
+++ src/main/java/euler/Euler3.java
@@ -7,29 +7,13 @@ import java.util.stream.IntStream;
 
 public class Euler3 {
 
-  public static IntPredicate isPrime = n -> n < 4
+  public static LongPredicate isPrime = n -> n < 4
       || !(n % 2 == 0 || IntStream.range(2, (int) Math.sqrt(n) + 1).filter(x -> x % 2 != 0 && n % x == 0).findAny()
           .isPresent());
 
   public static void main(String[] args) {
-    System.out.println(isPrime.test(5));
-    // TODO Auto-generated method stub
-    // primeFactor(20).stream().forEach(System.out::println);
-
-    int n = 6;
-    IntStream.range(2, (int) Math.sqrt(n) + 1).filter(x -> n % x == 0).filter(isPrime).forEach(System.out::println);
+    long n = 600851475143l;
+    System.out.print(LongStream.range(2, Math.round(Math.sqrt(n))).filter(x -> n % x == 0).filter(isPrime).max()
+        .getAsLong());
   }
-
-  static Set<Long> primeFactor(long num) {
-
-    Set<Long> factors = new HashSet<>();
-    for (long i = 2; i <= num; i++) {
-      while (num % i == 0) {
-        factors.add(i);
-        num /= i;
-      }
-    }
-    return factors;
-  }
-
 }
